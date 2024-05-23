b40f9c982a3a7278b85ed8957dbc7dcf21d2d47a
Anjina-devops <162683109+Anjina-devops@users.noreply.github.com>
2024-05-23 11:59:01 +0530

    Update create-servers.sh
    adding hosted zone id

--- create-servers.sh
+++ create-servers.sh
@@ -5,7 +5,7 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0b34d8689bd628e3f
 DOMAIN_NAME=devopsswitch.online
-HOSTED_ZONE_ID=Z0308214GYCUYHGJHT8R
+HOSTED_ZONE_ID=Z09589127RQPL86EUBK0
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
